d3c47c582b3b78a821b05ad7fc2d132388107ae8
workingSpear <anikaitsrivastav@gmail.com>
2026-02-22 20:41:36 -0800

    comments

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 // Code Practice: Slime World
-// Name: 
-// Date: 
+// Name: Kait Srivastav
+// Date: 02/20/2026
+// Game Title: Escape From The Lunarians / 月人から逃げ
+// Approximate Hours: 10 Hours
+// Credits: See Credits Page for asset credits. There are no non-example or non-documentation credits
+// Creative Tilt: Athetic: I am very happy with the enemy assets and I also added a particle trail on the player that begins when they reach their high score. It allows the player to easily figure out if they are at a new highscore without having to glance at the UI.
+// Creative TIlt: Technical: I learned how to use Phaser's "Groups" for arcade physics in order to create object pooling to better optimize the player's shots. I also used object pooling to manage my enemy data and I felt it quite condensed the code that way.
 
 let config = {
     type: Phaser.AUTO,
